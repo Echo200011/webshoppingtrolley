663609ab595f219cd74dc074493ed8e41c6ab412
19353854981 <zeyu.yang@baozun.com>
2023-03-06 18:32:30 +0800

    style:修改了schema和sql表

--- src/main/resources/db/migration/V1.0.0__2023.3.3_create_spu.sql
+++ src/main/resources/db/migration/V1.0.0__2023.3.3_create_spu.sql
@@ -1,30 +1,38 @@
-CREATE TABLE spu
+create table spu
 (
-    id       int                 not null primary key auto_increment comment '商品id',
-    `code`   varchar(255) unique not null comment '商品编码',
-    `name`   varchar(255)        not null comment '商品名称',
-    price    int                 not null comment '商品价格×1000后的价格',
-    discount int                 null comment '优惠后的标价×1000后的价格',
-    category int                 not null comment '商品种类'
+    id          int                 not null primary key auto_increment comment '商品id',
+    `code`      varchar(255) unique not null comment '商品编码',
+    `name`      varchar(255)        not null comment '商品名称',
+    price       int                 not null comment '商品价格×1000后的价格',
+    discount    int                 null comment '优惠后的标价×1000后的价格',
+    category_id int                 not null comment '商品种类',
+    index `idx_category_id` (category_id),
+    index `idx_name` (name)
 );
+
 create table promotion
 (
-    id            int primary key auto_increment                       not null comment '活动id',
-    `name`        varchar(255)                                         not null comment '活动名称',
+    id            int primary key auto_increment      not null comment '活动id',
+    `name`        varchar(255)                        not null comment '活动名称',
     type          enum ('DISCOUNT','GIFT','BUNDLING') not null comment '活动类型',
-    `description` varchar(2048) comment '活动描述',
-    detail        json                                                 not null comment '{"price":10,"discount":2}; {"spuId":1,"song":2}'
+    `description` varchar(2047)                       null comment '活动描述',
+    detail        json                                not null comment '放入活动规则如:+{"price":10,"discount":2}; {"spuId":1,"song":2}',
+    index `idx_name` (name)
 );
-create table merchandise_categories
+
+create table spu_categories
 (
     id        int          not null primary key auto_increment comment '种类id',
     `name`    varchar(255) not null comment '种类名称',
-    parent_id int          null comment '父级id'
+    parent_id int          null comment '父级id',
+    index `idx_parent_id` (parent_id),
+    index `idx_name` (name)
 );
+
 create table spu_promotion_mapping
 (
     id           int        not null primary key auto_increment comment '主键',
     spu_id       int        not null comment 'spu的id',
     promotion_id int unique not null comment 'promotion的id',
-    index `spu_promotion` (spu_id, promotion_id)
+    index `idx_spu_id_promotion_id` (spu_id, promotion_id)
 );
