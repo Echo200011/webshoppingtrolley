d927449fbe2c3fa2c604d42ade03a33878bc8276
19353854981 <zeyu.yang@baozun.com>
2023-03-13 19:01:53 +0800

    feat:商品状态修改

--- src/main/java/com/baozun/shoppingcart/dao/model/Promotion.java
+++ src/main/java/com/baozun/shoppingcart/dao/model/Promotion.java
@@ -6,14 +6,11 @@ import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import lombok.Data;
-import java.util.List;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
